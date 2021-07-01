c7b6d2f101218461e22103b8054c32d2eba6215c
gonzalomontero <montero.gonzalo90@gmail.com>
2021-07-01 00:18:52 -0300

    formatting exception handler

--- src/main/java/com/catalog/controller/handler/ApplicationExceptionHandler.java
+++ src/main/java/com/catalog/controller/handler/ApplicationExceptionHandler.java
@@ -1,16 +1,25 @@
 package com.catalog.controller.handler;
 
 import com.catalog.exception.ApplicationException;
+import com.google.gson.Gson;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
+
 public class ApplicationExceptionHandler {
 
-    @ExceptionHandler({ ApplicationException.class })
+    @ExceptionHandler({ApplicationException.class})
     public ResponseEntity<String> handleApplicationException(ApplicationException e) {
 
+        Gson gson = new Gson();
+        HashMap<String, String> response = new HashMap<>();
         HttpStatus httpStatus = HttpStatus.valueOf(e.getErrorCode().getHttpStatusCode());
-        return ResponseEntity.status(httpStatus).body(e.getMessage());
+
+        response.put("message", e.getMessage());
+        response.put("code", e.getErrorCode().getCode());
+
+        return ResponseEntity.status(httpStatus).body(gson.toJson(response));
     }
 }
